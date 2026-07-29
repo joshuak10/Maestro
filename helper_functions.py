5353ae1217ef6455ec0ed86e30d0a1913a94c2ae
Josh <dc.josh.kim@gmail.com>
2026-07-28 19:40:06 -0700

    split by harm_var instead of aribtrary 80/20 split

--- helper_functions.py
+++ helper_functions.py
@@ -10,7 +10,7 @@ REF_FREQ = 440
 class AudioRecordingDataset(Dataset):
     def __init__(self, path):
         data = np.load(path)
-        self.X, self.Y = data['X'], data['Y']
+        self.X, self.Y, self.hv = data['X'], data['Y'], data['hv']
 
 
     def __len__(self):
@@ -23,7 +23,7 @@ def midi_to_note(midi):
     return librosa.midi_to_note(midi)
 
 def amp_to_db(cqt, ref = 1):
-    return np.multiply(20, np.log10(cqt/ref))
+    return 20 * np.log10(np.maximum(np.abs(cqt), 1e-10)/ref)
 
 def midi_to_Hz(midi_note: int) -> float:
     return REF_FREQ * 2 ** ((midi_note - 69)/12)
@@ -71,8 +71,8 @@ def synth_note(midi_note: int, sr: int, duration: float, num_harmonics=3, amp=1,
     
     return y, freq
 
-def add_noise(x, snr = 0):
-    if snr == 0:
+def add_noise(x, snr = None):
+    if snr is None:
         return x
     signal_power = np.mean(x ** 2)
     raw_noise = np.random.normal(0,1, x.shape)
@@ -86,22 +86,27 @@ def add_noise(x, snr = 0):
     noisy_soundwave = x + scaled_noise
     return noisy_soundwave
 
+HARM_VAR = [0.0, 0.05, 0.1, 0.15, 0.25]
+SNR_LEVELS = [None, 0, 10, 20, 30]
 def generate_dataset(sr=44100, duration=2):
     dataset_x = []
     dataset_y = []
+    dataset_hv = []
     
     for midi_note in range(21, 109):
-        for harm_var in [0.0, 0.05, 0.1, 0.15]:
+        for harm_var in HARM_VAR:
             x, _ = synth_note(midi_note, sr, duration, num_harmonics=3, harmonic_variance=harm_var)
-            for snr in [0, 10, 20, 30]:
+            for snr in SNR_LEVELS:
                 x_new = add_noise(x, snr)
                 _, cqt_mean = extract_features(x_new, sr)  
                 dataset_x.append(cqt_mean)
                 dataset_y.append(midi_note - 21)  # 0–87
+                dataset_hv.append(harm_var)
     
     X = np.array(dataset_x)  # Shape: (num_samples, 84)
     Y = np.array(dataset_y)
-    return X, Y
+    hv = np.array(dataset_hv)
+    return X, Y, hv
 
 
 
