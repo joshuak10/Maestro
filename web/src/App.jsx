04e1d8e7fcdcd1111f92eea8fd9334425bfc4360
Josh <dc.josh.kim@gmail.com>
2026-08-06 10:23:49 -0700

    audioworklet, side thread implemented

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,55 @@
-import { useState } from 'react'
-
+import { useState, useRef,useEffect } from 'react'
 import tunerImg from './assets/tuner.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isOn, setIsOn]  = useState(false)
+  const audioCtxRef = useRef(null)
+  const streamRef = useRef(null)
+  const audioNode = useRef(null)
+  const [error, setError] = useState(null)
+  const audio = {
+    audio: {
+      channelCount: 1,
+      echoCancellation: false,
+      noiseSuprpression: false,
+      autoGainControl: false,
+    }
+  }
+
+  useEffect(() => {
+    if (!isOn) {
+      return
+    }
+    //use effect
+    let cancelled = false
+    const start = async() => {
+      try{
+        audioCtxRef.current = new AudioContext({sampleRate: 16000})
+        await audioCtxRef.current.audioWorklet.addModule('/pcm-processor.js')
+        if (cancelled) {audioCtxRef.current.close(); return}
+  
+        streamRef.current = await navigator.mediaDevices.getUserMedia(audio)
+        if (cancelled) {streamRef.current.getTracks().forEach(t=>t.stop()); audioCtxRef.current.close();return}
+        
+        audioNode.current = new AudioWorkletNode(audioCtxRef.current, 'pcm-processor')
+        audioNode.current.port.onmessage = (e) => console.log("here")
+        audioCtxRef.current.createMediaStreamSource(streamRef.current).connect(audioNode.current)
+      } catch (err) {
+        setError('Mic access denied')
+        setIsOn(false)
+      }
+    }
+    start()
+    //turn off
+    return () => {
+      cancelled = true
+      streamRef.current?.getTracks().forEach(t=>t.stop())
+      audioCtxRef.current?.close()
+      streamRef.current = null
+      audioCtxRef.current = null
+    }
+    
+  }, [isOn])
 
   return (
     <section id="center">
@@ -14,12 +60,13 @@ function App() {
         <div>
           <h1>Click the Button below to Tune!</h1>
         </div>
+        {error && <p className="error">{error}</p>}
         <button
           type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          className={isOn ? 'switch switch-on' : 'switch'}
+          onClick={() => setIsOn(prev => !prev)}
           >
-          Tuning Off
+          {isOn ? 'Stop Tuning' : 'Start Tuning'}
         </button>
       </div>
     </section>
