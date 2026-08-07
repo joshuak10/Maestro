0e530af5d956931b0e81f7df7b749edbd956b9bc
Josh <dc.josh.kim@gmail.com>
2026-08-06 18:52:30 -0700

    link jsx to API endpoint POST predict

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,14 @@
 import { useState, useRef,useEffect } from 'react'
 import tunerImg from './assets/tuner.png'
 
+function display(result){
+  if (!result || result.predictions.length === 0) return "listening..."
+  if (result.low_confidence) return 'low confidence'
+  return result.predictions[0].note
+}
+
+const API = import.meta.env.DEV ? 'https//localhost:8000' : ""
+
 function App() {
   const [isOn, setIsOn]  = useState(false)
   const audioCtxRef = useRef(null)
@@ -11,10 +19,12 @@ function App() {
     audio: {
       channelCount: 1,
       echoCancellation: false,
-      noiseSuprpression: false,
+      noiseSuppression: false,
       autoGainControl: false,
     }
   }
+  const inFlightRef = useRef(false)
+  const [result, setResult] = useState(null)
 
   useEffect(() => {
     if (!isOn) {
@@ -32,7 +42,24 @@ function App() {
         if (cancelled) {streamRef.current.getTracks().forEach(t=>t.stop()); audioCtxRef.current.close();return}
         
         audioNode.current = new AudioWorkletNode(audioCtxRef.current, 'pcm-processor')
-        audioNode.current.port.onmessage = (e) => console.log("here")
+        audioNode.current.port.onmessage = async (e) => {
+        
+          if (inFlightRef.current) return
+          inFlightRef.current = true
+          try{
+            const res = await fetch(`${API}/predict?sr=16000`, {
+              method: 'POST',
+              body: e.data.buffer,
+            })
+            if (!res.ok) return
+            setResult(await res.json())
+          } catch{
+
+          } finally {
+            inFlightRef.current = false
+          }
+        }
+        
         audioCtxRef.current.createMediaStreamSource(streamRef.current).connect(audioNode.current)
       } catch (err) {
         setError('Mic access denied')
@@ -47,6 +74,8 @@ function App() {
       audioCtxRef.current?.close()
       streamRef.current = null
       audioCtxRef.current = null
+      inFlightRef.current = false
+      setResult(null)
     }
     
   }, [isOn])
@@ -60,6 +89,7 @@ function App() {
         <div>
           <h1>Click the Button below to Tune!</h1>
         </div>
+        {isOn && <p>{display(result)}</p>}
         {error && <p className="error">{error}</p>}
         <button
           type="button"
